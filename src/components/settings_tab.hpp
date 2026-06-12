a6c03e26d39464123f5aa8d9fea656e6b0fc9733
dewberryants <dewberryants@gmail.com>
2026-06-12 14:58:02 +0200

    basic UI framework

---
+++ src/components/settings_tab.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <ftxui/component/component.hpp>
+#include <ftxui/dom/elements.hpp>
+
+namespace game {
+    ftxui::Component MakeSettingsTab(bool& musicEnabled, int& volume) {
+        using namespace ftxui;
+
+        auto volume_sl = Slider("Volume:", volume, 0, 100, 1);
+        auto music_cb = Checkbox("Music", &musicEnabled);
+
+        auto container = Container::Vertical({
+            music_cb,
+            volume_sl
+            });
+
+        return Renderer(container, [music_cb, volume_sl] {
+            return vbox({
+                text("Settings") | bold | center,
+                filler(),
+                music_cb->Render() | center,
+                volume_sl->Render() | size(WIDTH, EQUAL, 32) | center,
+                filler()
+                }) | flex;
+            }) | size(WIDTH, EQUAL, 64) | center;
+    }
+} // namespace game
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,47 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/canvas.hpp>
+#include <components/settings_tab.hpp>
 
 
 int main() {
     using namespace ftxui;
 
+    bool musicEnabled = true;
+    int volume = 80;
+    int selectedTab = 0;
+
+
     ScreenInteractive screen = ScreenInteractive::FitComponent();
 
-    int selectedTab = 0;
+    Component exitButton = Button("Quit", screen.ExitLoopClosure(), ButtonOption::Ascii());
+
+    Component gameTab = Renderer([] {
+        Canvas c = Canvas(90, 22);
+        c.DrawText(0, 0, "This is the game renderer!");
+        return canvas(std::move(c));
+    });
+
+    Component creaturesTab = Renderer([] { return text("Creatures...") | center; });
+    Component inventoryTab = Renderer([] { return text("Inventory...") | center; });
+    Component playerTab = Renderer([] { return text("Player...") | center; });
+    Component settingsTab = game::MakeSettingsTab(musicEnabled, volume);
+
+    Component tab = Container::Tab({
+        gameTab,
+        creaturesTab,
+        playerTab,
+        inventoryTab,
+        settingsTab
+    }, &selectedTab);
+
     std::vector<std::string> tabTitles = {
+        "| Game",
         "| Creatures",
         "| Player",
         "| Inventory",
         "| Settings"
     };
-
-    Component exitButton = Button("Quit", screen.ExitLoopClosure(), ButtonOption::Ascii());
-
-    Component gameRenderer = Renderer([&] {
-        Canvas c = Canvas(100, 100);
-        c.DrawText(0, 0, "This is the game renderer!");
-        return canvas(std::move(c));
-    });
-
-    Component tab = Container::Tab({
-        gameRenderer,
-        gameRenderer,
-        gameRenderer,
-        gameRenderer
-    }, &selectedTab);
 
     MenuOption customMenu = MenuOption::Vertical();
     customMenu.elements_prefix = [] { return text("-O"); };
@@ -49,12 +61,12 @@ int main() {
         return vbox({
             hbox({
                 tab->Render() | flex | border,
-                tabToggle->Render() | flex
-            }),
+                tabToggle->Render()
+            }) | size(HEIGHT, EQUAL, 25) | size(WIDTH, EQUAL, 105),
             hbox({
-                text("AsciiMon v.0.0.1") | bold, filler(), exitButton->Render() | bold
+                text("AsciiMon v.0.0.1") | dim, filler(), exitButton->Render() | bold
             })
-        });
+         });
     });
   
     screen.Loop(mainRenderer);
